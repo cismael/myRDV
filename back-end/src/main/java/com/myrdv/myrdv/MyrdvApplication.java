0ca266cef1aeb583888f084f5a02afb64fe4f8c4
walter-da-costa <walter.paquete@gmail.com>
2018-09-09 21:44:24 +0200

    init back-end

---
+++ back-end/src/main/java/com/myrdv/myrdv/MyrdvApplication.java
@@ -0,0 +1,12 @@
+package com.myrdv.myrdv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyrdvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyrdvApplication.class, args);
+	}
+}
