630d5d82ee896cb7c9b1024f62781b1ad22c80cc
Ismaël COULIBALY <c.ismael@live.fr>
2018-09-23 22:41:25 +0200

    Updating rest controller

--- back-end/src/main/java/com/myrdv/controller/HelloApplication.java
+++ back-end/src/main/java/com/myrdv/controller/HelloApplication.java
@@ -1,13 +1,24 @@
 package com.myrdv.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class HelloApplication {
 
-	@RequestMapping("/hello")
-	public String sayHello(){
-		return "Hello from the back-end";
-	}
+    private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String sayHello() {
+        LOGGER.debug("calling method : greet");
+        return "Hello from the back-end";
+    }
+
 }
