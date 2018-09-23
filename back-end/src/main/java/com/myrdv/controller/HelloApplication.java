b4dc069480fc6e3b19f3f5301cb0ab2110eff3ea
Ismaël COULIBALY <c.ismael@live.fr>
2018-09-23 23:44:15 +0200

    Adding front-end call authorization to back-end

--- back-end/src/main/java/com/myrdv/controller/HelloApplication.java
+++ back-end/src/main/java/com/myrdv/controller/HelloApplication.java
@@ -4,13 +4,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:4200")
 public class HelloApplication {
 
     private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
