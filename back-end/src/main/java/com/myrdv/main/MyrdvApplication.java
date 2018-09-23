5abdf7ed03de493cba42612046f9e14844192b0c
Ismaël COULIBALY <c.ismael@live.fr>
2018-09-23 21:07:55 +0200

    change package for ignored test

--- back-end/src/main/java/com/myrdv/main/MyrdvApplication.java
+++ back-end/src/main/java/com/myrdv/main/MyrdvApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MyrdvApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MyrdvApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MyrdvApplication.class, args);
+    }
 }
