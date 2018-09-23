bf6ec89799aba9ff9bc8331f647545aa3e94024b
Ismaël COULIBALY <c.ismael@live.fr>
2018-09-23 23:43:14 +0200

    Calling the REST API call service from our base module

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+// My Services
+import {MyServiceService} from './my-service.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  title = 'WAZAAAAA';
+  myVar = '';
+
+  constructor(private myApiService: MyServiceService) {
+  }
+
+  /**
+   * Initialisation du composant
+   */
+  ngOnInit() {
+    // On initialise la carte Google Map
+    this.sayHello();
+  }
+
+  sayHello() {
+    this.myApiService.getHello()
+      .subscribe(
+        response => { this.myVar = JSON.stringify(response).trim().valueOf();
+        },
+        error => {
+          console.log('AppComponent Error : ', error);
+        }
+      )
+  }
+
 }
