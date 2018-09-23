5f63e4b8a23936be443eaeabec320b6e611c1d1d
Ismaël COULIBALY <c.ismael@live.fr>
2018-09-23 22:41:08 +0200

    adding base package to scan by spring

--- back-end/src/main/java/com/myrdv/main/MyrdvApplication.java
+++ back-end/src/main/java/com/myrdv/main/MyrdvApplication.java
@@ -3,7 +3,7 @@ package com.myrdv.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.myrdv"})
 public class MyrdvApplication {
 
     public static void main(String[] args) {
